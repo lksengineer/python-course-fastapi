eb40289822d938337833154f0aad70144fe048ed
lksengineer <ksaavedrasu@gmail.com>
2023-09-26 18:12:48 -0500

    class2-documentation-swagger

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # Project-related libraries (installed via pip)
 from fastapi import FastAPI
 
+
 app = FastAPI()
+app.title = "My Movie app with FastAPI"
+app.version = "0.0.1"
 
 
-@app.get('/')
+@app.get('/', tags=["home"])
 def message():
     return "Hello World!"
