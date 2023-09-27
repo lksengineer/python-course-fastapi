24bd330f4f29061c05171497f2fba4395f1fb178
lksengineer <ksaavedrasu@gmail.com>
2023-09-26 23:34:36 -0500

    class-4-path-parameters

--- main.py
+++ main.py
@@ -21,16 +21,35 @@ movies = [
         "year": "2009",
         "rating": 7.8,
         "category": "Acción"
+    },
+    {
+        "id": 2,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
     }
 ]
 
 
 @app.get('/', tags=["home"])
 def message():
+    """Message function"""
     return HTMLResponse("<h1>Hello world!</h1>")
     # return {"name": "Luis", "last_name": "Saavedra", "age": 33}
 
 
-@app.get('/movie', tags=["movies"])
-def get_movie():
+@app.get('/movies', tags=["movies"])
+def get_movies():
+    """Get movie function"""
     return movies
+
+
+@app.get('/movies/{id}', tags=["movies"])
+def get_movie(id: int):
+    """Get movie function"""
+    for item in movies:
+        if item["id"] == id:
+            return item
+    return []
